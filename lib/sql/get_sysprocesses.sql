99641209671c86223f49a49b9a55a528e160321e
Jim Clouse <jclouse@glgroup.com>
2015-04-18 13:20:26 -0400

    more data in sys process query, filtering by sleeping

--- lib/sql/get_sysprocesses.sql
+++ lib/sql/get_sysprocesses.sql
@@ -1,3 +1,15 @@
-select spid, blocked, last_batch, hostname, program_name 
-from sysprocesses 
+select  s.spid
+        ,rtrim(ltrim(s.status)) as status
+        ,rtrim(ltrim(s.cmd)) as cmd
+        ,s.blocked
+        ,s.last_batch as lastBatch
+        ,rtrim(ltrim(d.name)) as databaseName
+        ,rtrim(ltrim(s.hostname)) as hostname
+        ,s.program_name as programName
+        ,s.cpu
+        ,s.physical_io as physicalIo
+        ,s.memusage as memUsage
+from    sysprocesses s 
+    left join   sysdatabases d
+        on          s.dbid = d.dbid
 order by spid;
