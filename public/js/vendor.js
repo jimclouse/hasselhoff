8cc018c74214c2dc06ff1c07db638a6cbeee6c3b
Jim Clouse <jclouse@glgroup.com>
2015-04-18 11:27:08 -0400

    including moment.js in project

--- public/js/vendor.js
+++ public/js/vendor.js
@@ -1,5 +1,6 @@
-$ = jQuery = require('../../bower_components/jquery/dist/jquery.js')
-require('../../bower_components/bootstrap/dist/js/bootstrap.js')
-require('../../bower_components/angular/angular.js')
-require('../../bower_components/angular-route/angular-route.js')
-_ = require('lodash')
+$ = jQuery = require('../../bower_components/jquery/dist/jquery.js');
+require('../../bower_components/bootstrap/dist/js/bootstrap.js');
+require('../../bower_components/angular/angular.js');
+require('../../bower_components/angular-route/angular-route.js');
+moment = require('../../bower_components/momentjs/min/moment.min.js');
+_ = require('lodash');
