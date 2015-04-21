8353882468e638c25388d754ac8578e2052f7bc7
Jim Clouse <jclouse@glgroup.com>
2015-04-21 08:21:47 -0400

    listing all index stats updated dates

---
+++ lib/sql/get_allStatsDates.sql
@@ -0,0 +1,19 @@
+USE {{database}};
+SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+
+SELECT  s.name AS schemaName
+        ,t.name AS tableName
+        ,i.name AS indexName
+        ,STATS_DATE(i.id,i.indid) AS lastUpdateDate
+        ,i.rowcnt AS indexCount
+        ,i.rowmodctr AS numberChanges
+        ,CAST((CAST(i.rowmodctr AS DECIMAL(28,8))/CAST(i.rowcnt AS DECIMAL(28,2)) * 100.0) AS DECIMAL(28,2)) AS pctRowsChanged
+FROM    sys.sysindexes i
+  JOIN    sys.tables t
+    ON      t.object_id = i.id
+  JOIN    sys.schemas s
+    ON      s.schema_id = t.schema_id
+WHERE   i.id > 100
+  AND     i.indid > 0
+  AND     i.rowcnt >= 500
+ORDER BY lastUpdateDate, schemaName, tableName, indexName
