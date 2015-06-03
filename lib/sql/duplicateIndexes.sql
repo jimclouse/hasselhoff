a86703ec01d34341dd26b321a30961a49e1314c1
Jim Clouse <jclouse@glgroup.com>
2015-06-03 18:48:24 -0400

    adding page for exact duplicate indexes

---
+++ lib/sql/duplicateIndexes.sql
@@ -0,0 +1,31 @@
+USE {{database}};
+SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
+
+with cte_indexCols as (
+    select  object_id as id, index_id as indid, name,
+            (   select case keyno when 0 then NULL else colid end as [data()]
+                from sys.sysindexkeys as k
+                where k.id = i.object_id
+                    and     k.indid = i.index_id
+                order by keyno, colid
+                for xml path('')
+            ) as cols,
+            (   select case keyno when 0 then colid else NULL end as [data()]
+                from sys.sysindexkeys as k
+                where k.id = i.object_id
+                    and k.indid = i.index_id
+                order by colid
+                for xml path('')
+            ) as inc
+    from sys.indexes as i
+)
+select  object_schema_name(c1.id) AS schemaName
+        ,object_name(c1.id) As tableName
+        ,c1.name as indexName
+        ,c2.name as duplicateIndexName
+from    cte_indexCols as c1
+    join    cte_indexCols as c2
+        on      c1.id = c2.id
+            and     c1.indid < c2.indid
+            and     c1.cols = c2.cols
+            and     c1.inc = c2.inc;
