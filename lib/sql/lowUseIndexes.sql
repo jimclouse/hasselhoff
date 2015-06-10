b62b302f86b2baab75c1c315eafa16593cd10868
Jim Clouse <jclouse@glgroup.com>
2015-06-10 09:55:57 -0400

    adding user seeks, scans, and lookups

--- lib/sql/lowUseIndexes.sql
+++ lib/sql/lowUseIndexes.sql
@@ -4,11 +4,14 @@ SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
 SELECT top 100 SCHEMA_NAME(o.Schema_ID) AS schemaName
     ,OBJECT_NAME(s.[object_id]) AS tableName
     ,i.name AS indexName
-    ,s.user_updates as updates
-    ,s.system_seeks as seeks
-    ,s.system_scans as scans
-    ,s.system_lookups as lookups
-    ,s.system_seeks + s.system_scans + s.system_lookups as totalUsage
+    ,S.user_updates as updates
+    ,S.user_seeks as userSeeks
+    ,S.user_scans as userScans
+    ,S.user_lookups as userLookups
+    ,S.system_seeks as systemSeeks
+    ,S.system_scans as systemScans
+    ,S.system_lookups as systemLookups
+    ,S.system_seeks + s.system_scans + s.system_lookups + S.user_seeks + S.user_scans + S.user_lookups as totalUsage
 FROM    sys.dm_db_index_usage_stats s
     JOIN    sys.indexes i
         ON      s.[object_id] = i.[object_id]
@@ -17,8 +20,6 @@ FROM    sys.dm_db_index_usage_stats s
         ON      i.object_id = O.object_id
 WHERE   s.database_id = DB_ID()
     AND     OBJECTPROPERTY(s.[object_id], 'IsMsShipped') = 0
-    AND s.user_seeks = 0
-    AND s.user_scans = 0
-    AND s.user_lookups = 0
-    AND i.name IS NOT NULL
-ORDER BY s.user_updates DESC;
+    AND     S.system_seeks + s.system_scans + s.system_lookups + S.user_seeks + S.user_scans + S.user_lookups > 0
+    AND     i.name IS NOT NULL
+ORDER BY S.system_seeks + s.system_scans + s.system_lookups + S.user_seeks + S.user_scans + S.user_lookups ASC;
--- lib/sql/neverUsedIndexes.sql
+++ lib/sql/neverUsedIndexes.sql
@@ -5,10 +5,13 @@ SELECT  SCHEMA_NAME(O.Schema_ID) AS schemaName
         ,OBJECT_NAME(I.object_id) AS tableName
         ,I.NAME AS indexName
         ,S.user_updates as updates
-        ,S.system_seeks as seeks
-        ,S.system_scans as scans
-        ,S.system_lookups as lookups
-        ,S.system_seeks + s.system_scans + s.system_lookups as totalUsage
+        ,S.user_seeks as userSeeks
+        ,S.user_scans as userScans
+        ,S.user_lookups as userLookups
+        ,S.system_seeks as systemSeeks
+        ,S.system_scans as systemScans
+        ,S.system_lookups as systemLookups
+        ,S.system_seeks + s.system_scans + s.system_lookups + S.user_seeks + S.user_scans + S.user_lookups as totalUsage
 FROM    sys.indexes I 
     JOIN    sys.objects O
         ON      I.object_id = O.object_id
@@ -17,5 +20,5 @@ LEFT JOIN   sys.dm_db_index_usage_stats S
             AND     I.index_id = S.index_id
 WHERE   OBJECTPROPERTY(O.object_id,'IsMsShipped') = 0
     AND     I.name IS NOT NULL
-    AND     (S.object_id IS NULL OR (s.system_seeks + s.system_scans + s.system_lookups)  = 0 )
+    AND     (S.object_id IS NULL OR (S.system_seeks + s.system_scans + s.system_lookups + S.user_seeks + S.user_scans + S.user_lookups)  = 0 )
 ORDER BY S.user_updates desc, SchemaName, TableName, IndexName;
