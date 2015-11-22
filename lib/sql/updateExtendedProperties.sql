c45c40ef0baf9c086cf241ae0749dc077401c00b
Jim Clouse <jclouse@glgroup.com>
2015-11-22 11:59:40 -0500

    notes

--- lib/sql/updateExtendedProperties.sql
+++ lib/sql/updateExtendedProperties.sql
@@ -1,6 +1,8 @@
 USE {{database}};
 SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
 
+# login must have ALTER permissions on all the tables for this script to work
+
 IF EXISTS (select 1 from sys.extended_properties where major_id = {{objectId}} {{#columnId}} and minor_id = {{columnId}} {{/columnId}})
 BEGIN
 	EXEC sp_updateextendedproperty @name = 'MS_Description', @value = '{{{description}}}',
