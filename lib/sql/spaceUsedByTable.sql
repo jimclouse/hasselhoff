e5ba27ae9c1e55faf4c5ada846c5571e5873549e
Jim Clouse <jclouse@glgroup.com>
2016-05-31 08:40:35 -0400

    completed space used by table query

--- lib/sql/spaceUsedByTable.sql
+++ lib/sql/spaceUsedByTable.sql
@@ -2,11 +2,26 @@ USE {{database}};
 SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
 SET NOCOUNT ON;
 
-select  t.name
-        ,4000 as rows
-        ,100 as reserved
-        ,70 as data
-        ,30 as indexes
-        ,5 as unused
-from sys.tables t
-where type = 'u';
+create table #usage (name sysname, rows int, reserved varchar(64), data varchar(64), indexSize varchar(64), unused varchar(64))
+
+exec sp_msforeachtable 'insert into #usage exec sp_spaceused ''?'' '
+
+update #usage set reserved = LTRIM(RTRIM(REPLACE(reserved,'KB','')))
+update #usage set data = LTRIM(RTRIM(REPLACE(data,'KB','')))
+update #usage set indexSize = LTRIM(RTRIM(REPLACE(indexSize,'KB','')))
+update #usage set unused = LTRIM(RTRIM(REPLACE(unused,'KB','')))
+
+alter table #usage alter column reserved int
+alter table #usage alter column data int
+alter table #usage alter column indexSize int
+alter table #usage alter column unused int
+
+select  name
+    ,rows
+    ,reserved/1024.0 as reserved
+    ,data/1024.0 as data
+    ,indexSize/1024.0 as indexSize
+    ,unused as unused
+from #usage order by reserved desc;
+
+drop table #usage
